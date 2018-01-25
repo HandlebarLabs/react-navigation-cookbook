40e0bd968274d7593b83e6b38204ceaea3a52e9c
Spencer Carli <spencercarli@gmail.com>
2018-01-25 14:04:47 -0600

    Auth flow example

--- App/index.js
+++ App/index.js
@@ -1,5 +1,6 @@
 // import App from './stacknavigator-modal';
 // import App from './react-native-modal';
-import App from './modal-from-tab-bar';
+// import App from './modal-from-tab-bar';
+import App from './auth-flow';
 
 export default App;
