8c0585b284264b90f25edbd8d3a8ce0eea5eaa67
Spencer Carli <spencercarli@gmail.com>
2018-01-25 11:58:25 -0600

    Modal from tab bar example

--- App/index.js
+++ App/index.js
@@ -1,4 +1,5 @@
 // import App from './stacknavigator-modal';
-import App from './react-native-modal';
+// import App from './react-native-modal';
+import App from './modal-from-tab-bar';
 
 export default App;
