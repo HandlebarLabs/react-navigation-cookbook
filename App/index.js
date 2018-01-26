1db125c687a06bb2e6ead95b888a3c30be575ba5
Spencer Carli <spencercarli@gmail.com>
2018-01-26 10:38:13 -0600

    Custom transition example

--- App/index.js
+++ App/index.js
@@ -2,6 +2,7 @@
 // import App from './react-native-modal';
 // import App from './modal-from-tab-bar';
 // import App from './auth-flow';
-import App from './redux-integration';
+// import App from './redux-integration';
+import App from './custom-transition';
 
 export default App;
