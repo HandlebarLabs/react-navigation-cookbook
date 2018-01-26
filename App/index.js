d98b9a13b759e067fab7ca5ad50a4d9200333bb5
Spencer Carli <spencercarli@gmail.com>
2018-01-26 10:54:43 -0600

    Replace screen example

--- App/index.js
+++ App/index.js
@@ -3,6 +3,7 @@
 // import App from './modal-from-tab-bar';
 // import App from './auth-flow';
 // import App from './redux-integration';
-import App from './custom-transition';
+// import App from './custom-transition';
+import App from './replace-screen';
 
 export default App;
